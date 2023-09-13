cc86e080b273b488523bf8fa64b00cfeb3834e9d
Mohamed Hamouda <mohamed.hamouda7897@gmail.com>
2023-09-13 19:32:10 +0300

    create loadHadeth function and split ahadeth

--- lib/tabs/ahadeth.dart
+++ lib/tabs/ahadeth.dart
@@ -1,12 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:islami_c9_sunday/hadeth_model.dart';
 
 class AhadethTab extends StatelessWidget {
-  const AhadethTab({super.key});
+  List<HadethModel> allAhadeth = [];
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.amberAccent,
+    loadHadeth();
+    return Column(
+      children: [],
     );
+  }
+
+  void loadHadeth() {
+    rootBundle.loadString("assets/files/ahadeth.txt").then((ahadeth) {
+      List<String> ahadethList = ahadeth.split("#");
+
+      for (int i = 0; i < ahadethList.length; i++) {
+        String HadethOne = ahadethList[i];
+        List<String> hadethOneLines = HadethOne.trim().split("\n");
+        String title = hadethOneLines[0];
+        hadethOneLines.removeAt(0);
+        List<String> content = hadethOneLines;
+        HadethModel hadethModel = HadethModel(title, content);
+        print(hadethModel.title);
+        print("$i");
+        allAhadeth.add(hadethModel);
+      }
+    }).catchError((e) {
+      print(e.toString());
+    });
   }
 }
